d64690c853b2e006c27bcbd149c9fb49d8b63e40
adrijanat <63561072+adrijanat@users.noreply.github.com>
2020-05-09 15:52:15 +0200

    Add files via upload

--- react-journalist/src/components/article/ListArticleElement.jsx
+++ react-journalist/src/components/article/ListArticleElement.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 class ListArticleElement extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            article : {},
+            category : '',
+            authors : []
         };
     }
 
     componentDidMount() {
-        var article = this.props.article;
-        var timestamp = article.datelastmodified;
-        article.datelastmodified = new Date(timestamp).toUTCString();
-        this.setState({ article });
+        // get article's authors
+        axios.get('http://localhost:8080/articles/'+this.props.article.articleid+"/authors")
+            .then(res => {
+                const authors = res.data._embedded.authors;
+                this.setState({ authors });
+            });
+
+        // get article's category
+        axios.get('http://localhost:8080/articles/'+this.props.article.articleid+'/category')
+            .then(res => {
+                const category = res.data.name;
+                this.setState({ category });
+            });
     }
 
     render() {
@@ -26,7 +37,7 @@ class ListArticleElement extends Component {
 
                 <div className="col-md-8">
                     <a href={"/articles/"+this.props.article.articleid}><h3>{this.props.article.title}</h3></a>
-                    <p><span className="category">News</span> | {this.props.article.datelastmodified}</p>
+                    <p><span className="category">{this.state.category}</span> | by { this.state.authors.map(author => <a href={author._links.self.href.substring(21)}>{author.name}</a>,) } | {new Date(this.props.article.datelastmodified).toUTCString()}</p>
                     <p>{this.props.article.summary}</p>
                 </div>
             </div>
