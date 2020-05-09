728868ca9f3405d440f71067a452759254128860
adrijanat <63561072+adrijanat@users.noreply.github.com>
2020-05-09 19:16:24 +0200

    Add files via upload

--- react-journalist/src/components/article/ViewArticleStories.jsx
+++ react-journalist/src/components/article/ViewArticleStories.jsx
@@ -1,57 +1,70 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 class ViewArticleStories extends Component {
-    render() {
-        return (
-            <div>
-                <h4>ARTICLE:<br/>Burglary stories: ‘I slept with the light on for a year afterwards’</h4><br/>
-                <h4>QUESTION:<br/>Have you been a victim of a burglary?</h4><br/>
-                <h4>STORIES(3):</h4><br/>
+    constructor(props) {
+        super(props);
+        this.state = {
+            stories : []
+        };
+    }
 
-                <div className="row">
-                    <div className="col-md-12">
-                        <table className="table-bordered">
+    componentDidMount() {
+        // get article's stories
+        axios.get('http://localhost:8080/articles/' + this.props.match.params.id + "/stories")
+            .then(res => {
+                const stories = res.data._embedded.stories;
+                this.setState({stories});
+            });
+    }
+
+    render() {
+    return (
+        <div>
+            <table className="table table-bordered">
+                <tr>
+                    <td>Article</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Question</td>
+                    <td></td>
+                </tr>
+            </table>
+            <br/>
+            <h4>STORIES ({this.state.stories.length}):</h4>
+            {
+                this.state.stories.map(
+                    story =>
+                        <table className="table table-bordered">
                             <tr style={{"backgroundColor":"#ddd"}}>
-                                <td><b>Story id</b></td>
-                                <td>1</td>
+                                <td colSpan="2"><b>Story</b></td>
                             </tr>
                             <tr>
                                 <td><b>Name</b></td>
-                                <td>Laura Jones</td>
+                                <td>{story.name}</td>
                             </tr>
                             <tr>
                                 <td><b>Email</b></td>
-                                <td>laura@jones.com</td>
+                                <td>{story.email}</td>
                             </tr>
                             <tr>
                                 <td><b>Location</b></td>
-                                <td>Dublin</td>
+                                <td>{story.location}</td>
                             </tr>
                             <tr>
                                 <td><b>Summary</b></td>
-                                <td>The first time the guy apologized</td>
+                                <td>{story.summary}</td>
                             </tr>
                             <tr>
                                 <td><b>Story</b></td>
-                                <td>
-                                    <p>"My house in D8 was three times broken into in around one year. The first time we
-                                        were all inside and the guy opened one of the room’s doors and even apologised
-                                        to the person that was inside [who] thought it was just a friend trying to find
-                                        the toilet as she was sleeping."</p>
-                                    <p>"Another time, they stole a bike entering through the window when I was inside in
-                                        daylight. He had less than 10 minutes to do it. The third time they stole
-                                        everything they could including five laptops. The gardaí (that were the three
-                                        times very nice but zero efficient) told me to make changes to the house and I
-                                        did."</p>
-                                </td>
+                                <td>{story.body}</td>
                             </tr>
                         </table>
-
-                    </div>
-                </div>
-
-            </div>
-        )
+                )
+            }
+        </div>
+    )
     }
 }
 
