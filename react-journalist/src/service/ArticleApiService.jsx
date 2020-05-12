a1bc9a0436e43f84848fd3d8d3b790ac0faa1ff4
adrijanat <63561072+adrijanat@users.noreply.github.com>
2020-05-12 12:00:47 +0200

    Add files via upload

--- react-journalist/src/service/ArticleApiService.jsx
+++ react-journalist/src/service/ArticleApiService.jsx
@@ -1,36 +1,79 @@
 import axios from 'axios';
 
-const BASE = 'http://localhost:8080/articles/';
+const BASE = 'http://localhost:8080/';
+const BASE_A = 'http://localhost:8080/articles/';
 
 class ApiService {
 
     fetchArticleAuthors(id){
-        return axios.get(BASE + id + "/authors");
+        return axios.get(BASE_A + id + "/authors");
     }
 
     fetchArticleCategory(id){
-        return axios.get(BASE + id + "/category");
+        return axios.get(BASE_A + id + "/category");
     }
 
     fetchArticleComments(id){
-        return axios.get(BASE + id + "/comments");
+        return axios.get(BASE_A + id + "/comments");
     }
 
     fetchArticleKeywords(id){
-        return axios.get(BASE + id + "/keywords");
+        return axios.get(BASE_A + id + "/keywords");
     }
 
     fetchArticleStories(id){
-        return axios.get(BASE + id + "/stories");
+        return axios.get(BASE_A + id + "/stories");
     }
 
     fetchLatestInCat(catid) {
-        return axios.get(BASE + "latestincat/" + catid);
+        return axios.get(BASE_A + "latestincat/" + catid);
     }
 
     // latest / most viewed / most upvoted
     fetchPick(pick) {
-        return axios.get(BASE + pick);
+        return axios.get(BASE_A + pick);
+    }
+
+
+    addComment(articleid, commentid){
+        //return axios.post(BASE_A + articleid + "/comments", BASE + "comments/" + commentid);
+
+        return axios.post(
+            BASE_A + articleid + "/comments",
+            BASE + "comments/" + commentid,
+            {headers: {"Content-Type": "text/uri-list"}}
+        )
+    }
+
+    addAuthors(articleid, authorids){
+        for(const authorid in authorids) {
+            //return axios.post(BASE_A + articleid + "/authors", BASE + "authors/" + authorid);
+            return axios.post(
+                BASE_A + articleid + "/authors",
+                BASE + "authors/" + authorid,
+                {headers: {"Content-Type": "text/uri-list"}}
+            )
+        }
+    }
+
+    addKeywords(articleid, keywordids){
+        for(const keywordid in keywordids) {
+            //return axios.post(BASE_A + articleid + "/keywords", BASE + "keywords/" + keywordid);
+            return axios.post(
+                BASE_A + articleid + "/keywords",
+                BASE + "keywords/" + keywordid,
+                {headers: {"Content-Type": "text/uri-list"}}
+            )
+        }
+    }
+
+    addToCategory(categoryid, articleid){
+        //return axios.post(BASE + "category/" + categoryid + "/articles", BASE_A + articleid);
+        return axios.post(
+            BASE + "category/" + categoryid + "/articles",
+            BASE_A + articleid,
+            {headers: {"Content-Type": "text/uri-list"}}
+        )
     }
 }
 
