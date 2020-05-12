b4058d82d5d8edc8e2f9a64f1305615a39a1dd2b
adrijanat <63561072+adrijanat@users.noreply.github.com>
2020-05-12 12:04:48 +0200

    Add files via upload

--- react-journalist/src/components/comment/PostComment.jsx
+++ react-journalist/src/components/comment/PostComment.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ApiService from "../../service/ApiService";
+import ArticleApiService from "../../service/ArticleApiService";
+import App from "../../App";
 
 export default class PostComment extends Component {
     constructor(props){
@@ -21,10 +23,14 @@ export default class PostComment extends Component {
             body: this.state.body
         };
 
-        ApiService.add("comments", comment)
-            .then(res => {
-                this.setState({message: 'Comment posted.'});
-            });
+        ApiService.add("comments",comment)
+            .then(
+                res => {
+                    ArticleApiService.addComment(this.props.articleid, res.data.commentid);
+                    this.setState({message: 'Comment posted.'});
+                    window.location.reload();
+                }
+            )
     };
 
     onChange = (e) =>
@@ -35,26 +41,26 @@ export default class PostComment extends Component {
             <div style={{background:"#eee",padding:"30px"}}>
                 <form>
                     <div className="form-group row">
-                        <div className="col"/>
-                        <div className="col-sm-11">
+                        <div className="col-md-1"/>
+                        <div className="col-md-11">
                             All fields are required. Your email will not be published.
                         </div>
                     </div>
                     <div className="form-group row">
-                        <div className="col">Name</div>
-                        <div className="col-sm-11">
-                            <input type="text" required className="form-control" id="name" value={this.state.name} onChange={this.onChange}/>
+                        <div className="col-md-1">Name</div>
+                        <div className="col-md-11">
+                            <input type="text" required className="form-control" name="name" value={this.state.name} onChange={this.onChange}/>
                         </div>
                     </div>
                     <div className="form-group row">
-                        <div className="col">Email</div>
-                        <div className="col-sm-11">
-                            <input type="email" required className="form-control" id="email" value={this.state.email} onChange={this.onChange}/>
+                        <div className="col-md-1">Email</div>
+                        <div className="col-md-11">
+                            <input type="email" required className="form-control" name="email" value={this.state.email} onChange={this.onChange}/>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-12">
-                            <textarea id="body" required className="form-control" rows="5" value={this.state.body} onChange={this.onChange}/>
+                            <textarea name="body" required className="form-control" rows="5" value={this.state.body} onChange={this.onChange}/>
                         </div>
                     </div>
                     <br/>
