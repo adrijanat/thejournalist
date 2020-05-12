a4080f79168472cf3abd3179cb39a8cb22758848
adrijanat <63561072+adrijanat@users.noreply.github.com>
2020-05-12 10:56:48 +0200

    Add files via upload

--- react-journalist/src/components/author/AddAuthor.jsx
+++ react-journalist/src/components/author/AddAuthor.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from "axios";
 import ApiService from "../../service/ApiService";
 
 class AddAuthor extends Component{
@@ -9,7 +10,7 @@ class AddAuthor extends Component{
             name: '',
             email: '',
             bio: '',
-            //image: '',
+            image: '',
             message: null
         };
         this.saveAuthor = this.saveAuthor.bind(this);
@@ -21,7 +22,7 @@ class AddAuthor extends Component{
             name: this.state.name,
             email: this.state.email,
             bio: this.state.bio,
-           // image: this.state.image
+            image: this.state.image
         };
 
         ApiService.add("authors",author)
@@ -39,7 +40,7 @@ class AddAuthor extends Component{
                 <h2 className="text-center">Add Author</h2>
                 <form>
                     <div className="row">
-                    <div className="col-md-7">
+                    <div className="col-md-12">
                         <div className="form-group">
                             <label>Full Name</label>
                             <input type="text" name="name" className="form-control" value={this.state.name} onChange={this.onChange}/>
@@ -54,8 +55,14 @@ class AddAuthor extends Component{
                             <label>Bio</label>
                             <textarea rows="5" name="bio" className="form-control" value={this.state.bio} onChange={this.onChange}/>
                         </div>
-                    </div>
 
+                        <div className="form-group">
+                            <label>Image URL</label>
+                            <input type="text" name="image" className="form-control" value={this.state.image} onChange={this.onChange}/>
+                        </div>
+
+                    </div>
+                    {/*
                     <div className="col-md-5">
                         <div className="form-group">
                             <label>Image</label>
@@ -65,6 +72,7 @@ class AddAuthor extends Component{
                             <div style={{width:"100%", height:"200px", background:"#ddd"}}/>
                         </div>
                     </div>
+                    */}
                     </div>
                     <a className="btn btn-outline-secondary" href="/authors">Discard</a>
                     <button style={{float:"right"}} className="btn btn-outline-success" onClick={this.saveAuthor}>Add Author</button>
--- react-journalist/src/components/author/EditAuthor.jsx
+++ react-journalist/src/components/author/EditAuthor.jsx
@@ -63,48 +63,52 @@ class EditAuthor extends Component {
 
         ApiService.edit("authors", this.props.match.params.id, author)
             .then(res => {
-                this.setState({message : 'Author added successfully.'});
+                this.setState({message : 'Author updated successfully.'});
                 this.props.history.push('/authors');
             });
     };
 
     render() {
-        return (
+        return(
             <div>
-                <h2 className="text-center">Edit Author</h2>
+                <h2 className="text-center">Add Author</h2>
                 <form>
                     <div className="row">
-                        <div className="col-md-7">
-
-                    <div className="form-group">
-                        <label>Name</label>
-                        <input type="text" name="name" className="form-control" defaultValue={this.state.name}/>
-                    </div>
-
-                    <div className="form-group">
-                        <label>Email</label>
-                        <input name="firstName" className="form-control" value={this.state.email} onChange={this.onChange}/>
-                    </div>
-
-                    <div className="form-group">
-                        <label>Bio</label>
-                        <textarea rows="5" name="bio" className="form-control" value={this.state.bio} onChange={this.onChange}/>
-                    </div>
-                        </div>
-                            <div className="col-md-5">
-                                <div className="form-group">
-                                    <label>Image</label>
-                                    <br/>
-                                    <input type="file"/>
-                                    <br/><br/>
-                                    <div className="text-center">
-                                        <img src={this.state.image} style={{height:"200px", background:"#ddd"}}/>
-                                    </div>
-                                </div>
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <label>Full Name</label>
+                                <input type="text" name="name" className="form-control" value={this.state.name} onChange={this.onChange}/>
                             </div>
-                        </div>
 
-                    <a className="btn btn-outline-secondary" href={"/authors/"+this.state.authorid}>Discard</a>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input type="email" name="email" className="form-control" value={this.state.email} onChange={this.onChange}/>
+                            </div>
+
+                            <div className="form-group">
+                                <label>Bio</label>
+                                <textarea rows="5" name="bio" className="form-control" value={this.state.bio} onChange={this.onChange}/>
+                            </div>
+
+                            <div className="form-group">
+                                <label>Image URL</label>
+                                <input type="text" name="image" className="form-control" value={this.state.image} onChange={this.onChange}/>
+                            </div>
+
+                        </div>
+                        {/*
+                    <div className="col-md-5">
+                        <div className="form-group">
+                            <label>Image</label>
+                            <br/>
+                            <input type="file"/>
+                            <br/><br/>
+                            <div style={{width:"100%", height:"200px", background:"#ddd"}}/>
+                        </div>
+                    </div>
+                    */}
+                    </div>
+                    <a className="btn btn-outline-secondary" href="/authors">Discard</a>
                     <button style={{float:"right"}} className="btn btn-outline-success" onClick={this.saveAuthor}>Save</button>
                 </form>
             </div>
