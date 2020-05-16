a282c22f1faa06c5daf008b653a4441612dda55e
adrijanat <63561072+adrijanat@users.noreply.github.com>
2020-05-16 14:23:02 +0200

    Add files via upload

--- thejournalist/src/main/java/com/thejournalist/thejournalist/repository/ArticleRepository.java
+++ thejournalist/src/main/java/com/thejournalist/thejournalist/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package com.thejournalist.journalist.repository;
 
 import com.thejournalist.journalist.model.Article;
+import com.thejournalist.journalist.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,22 +14,22 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     /*default methods: save(), findOne(), findById(), findAll(), count(), delete(), deleteById() */
 
     // find by search
-    List<Article> findByTitleContainingIgnoreCaseOrSummaryContainingIgnoreCaseOrBodyContainingIgnoreCaseAndStatusEquals(String q1, String q2, String q3, String status);
+    List<Article> findByTitleContainingIgnoreCaseOrSummaryContainingIgnoreCaseOrBodyContainingIgnoreCaseAndStatusEquals(String q1, String q2, String q3, Status s);
 
     // find all starting with latest
     List<Article> findByOrderByDatecreatedDesc();
 
     // latest
-    List<Article> findFirst5ByAndStatusEqualsOrderByDatecreatedDesc(String status);
+    List<Article> findFirst5ByAndStatusEqualsOrderByDatecreatedDesc(Status s);
 
     // most viewed
-    List<Article> findFirst5ByAndStatusEqualsOrderByViewsDesc(String status);
+    List<Article> findFirst5ByAndStatusEqualsOrderByViewsDesc(Status s);
 
     // most upvoted
-    List<Article> findFirst5ByAndStatusEqualsOrderByUpvotesDesc(String status);
+    List<Article> findFirst5ByAndStatusEqualsOrderByUpvotesDesc(Status s);
 
     // latest in category
-    Optional<Article> findFirstByCategory_CategoryidAndStatusEquals(long id, String status);
+    Optional<Article> findFirstByCategory_CategoryidAndStatusEquals(long id, Status s);
 
     // increment upvotes
     @Transactional
